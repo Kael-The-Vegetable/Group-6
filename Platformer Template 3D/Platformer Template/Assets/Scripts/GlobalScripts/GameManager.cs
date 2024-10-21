cfbbde616e344fb5b6b8e685b6a5692b04659d14
Alex Twin <alex.twintheone@gmail.com>
2024-10-21 12:45:48 -0600

    Fixed your repository

--- Platformer Template 3D/Platformer Template/Assets/Scripts/GlobalScripts/GameManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameManager : MonoBehaviour
-{
-    // use this class to take care of all in game problems that might arrise such as keeping track of the time spent in the level or the number of keys collected, etc.
-}
